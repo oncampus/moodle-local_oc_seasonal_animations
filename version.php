bcb9c4fb48a3c80deeeda7e45c57c51fa8679f46
Konrad Ebel <konrad.ebel@oncampus.de>
2026-04-20 15:49:04 +0200

    Bump version

--- version.php
+++ version.php
@@ -27,7 +27,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_oc_seasonal_animations';
 $plugin->name = 'oncampus GmbH, Seasonal Animations';
-$plugin->version = 2026042001;
+$plugin->version = 2026042002;
 $plugin->requires = 2022041900;
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = 'v2.0.0';
+$plugin->release   = 'v2.1.0';
